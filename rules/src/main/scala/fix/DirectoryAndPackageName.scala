06b94b07b50daa908a149316c29fe45311866041
xuwei-k <6b656e6a69@gmail.com>
2022-02-12 07:37:47 +0900

    add DirectoryAndPackageNameConfig
    fix https://github.com/xuwei-k/scalafix-rules/issues/17

--- rules/src/main/scala/fix/DirectoryAndPackageName.scala
+++ rules/src/main/scala/fix/DirectoryAndPackageName.scala
@@ -1,8 +1,13 @@
 package fix
 
+import metaconfig.generic.Surface
+import metaconfig.ConfDecoder
+import metaconfig.Configured
 import scalafix.Diagnostic
 import scalafix.Patch
 import scalafix.lint.LintSeverity
+import scalafix.v1.Configuration
+import scalafix.v1.Rule
 import scalafix.v1.SyntacticDocument
 import scalafix.v1.SyntacticRule
 import scala.meta.Pkg
@@ -11,53 +16,93 @@ import scala.meta.Tree
 import scala.meta.inputs.Input
 import scala.meta.inputs.Position
 
-class DirectoryAndPackageName extends SyntacticRule("DirectoryAndPackageName") {
-  private[this] val keywords: Set[String] = Set(
-    "abstract",
-    "case",
-    "catch",
-    "class",
-    "def",
-    "do",
-    "else",
-    "enum",
-    "export",
-    "extends",
-    "false",
-    "final",
-    "finally",
-    "for",
-    "forSome",
-    "if",
-    "implicit",
-    "import",
-    "lazy",
-    "macro",
-    "match",
-    "ne",
-    "new",
-    "null",
-    "object",
-    "override",
-    "package",
-    "private",
-    "protected",
-    "return",
-    "sealed",
-    "super",
-    "then",
-    "this",
-    "throw",
-    "trait",
-    "try",
-    "true",
-    "type",
-    "val",
-    "var",
-    "while",
-    "with",
-    "yield",
-  ).map("`" + _ + "`")
+case class DirectoryAndPackageNameConfig(
+  baseDirectory: Seq[String],
+  keyword: Seq[String]
+)
+
+object DirectoryAndPackageNameConfig {
+  val default: DirectoryAndPackageNameConfig = DirectoryAndPackageNameConfig(
+    baseDirectory = Seq(
+      "/src/main/scala-2.11/",
+      "/src/test/scala-2.11/",
+      "/src/main/scala-2.12/",
+      "/src/test/scala-2.12/",
+      "/src/main/scala-2.13/",
+      "/src/test/scala-2.13/",
+      "/src/main/scala-2/",
+      "/src/test/scala-2/",
+      "/src/main/scala-3/",
+      "/src/test/scala-3/",
+      "/src/main/scala/",
+      "/src/test/scala/",
+    ),
+    keyword = Seq(
+      "abstract",
+      "case",
+      "catch",
+      "class",
+      "def",
+      "do",
+      "else",
+      "enum",
+      "export",
+      "extends",
+      "false",
+      "final",
+      "finally",
+      "for",
+      "forSome",
+      "if",
+      "implicit",
+      "import",
+      "lazy",
+      "macro",
+      "match",
+      "ne",
+      "new",
+      "null",
+      "object",
+      "override",
+      "package",
+      "private",
+      "protected",
+      "return",
+      "sealed",
+      "super",
+      "then",
+      "this",
+      "throw",
+      "trait",
+      "try",
+      "true",
+      "type",
+      "val",
+      "var",
+      "while",
+      "with",
+      "yield",
+    )
+  )
+
+  implicit val surface: Surface[DirectoryAndPackageNameConfig] =
+    metaconfig.generic.deriveSurface[DirectoryAndPackageNameConfig]
+
+  implicit val decoder: ConfDecoder[DirectoryAndPackageNameConfig] =
+    metaconfig.generic.deriveDecoder(default)
+}
+
+class DirectoryAndPackageName(config: DirectoryAndPackageNameConfig) extends SyntacticRule("DirectoryAndPackageName") {
+
+  def this() = this(DirectoryAndPackageNameConfig.default)
+
+  override def withConfiguration(config: Configuration): Configured[Rule] = {
+    config.conf
+      .getOrElse("DirectoryAndPackageName")(this.config)
+      .map(newConfig => new DirectoryAndPackageName(newConfig))
+  }
+
+  private[this] val keywords: Set[String] = config.keyword.map("`" + _ + "`").toSet
 
   override def isLinter = true
 
@@ -85,20 +130,7 @@ class DirectoryAndPackageName extends SyntacticRule("DirectoryAndPackageName") {
         path <- scalaSourceOpt
         // TODO Windows
         if !scala.util.Properties.isWin
-        dirOpt = Seq(
-          "/src/main/scala-2.11/",
-          "/src/test/scala-2.11/",
-          "/src/main/scala-2.12/",
-          "/src/test/scala-2.12/",
-          "/src/main/scala-2.13/",
-          "/src/test/scala-2.13/",
-          "/src/main/scala-2/",
-          "/src/test/scala-2/",
-          "/src/main/scala-3/",
-          "/src/test/scala-3/",
-          "/src/main/scala/",
-          "/src/test/scala/"
-        ).find { dir =>
+        dirOpt = config.baseDirectory.find { dir =>
           path.contains(dir)
         }.map { dir =>
           path.split(dir).last.split('/').init.mkString("/")
