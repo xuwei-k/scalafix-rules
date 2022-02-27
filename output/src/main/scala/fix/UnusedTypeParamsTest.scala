8409d2fad65b78083b33ff96202018e1f28fe455
xuwei-k <6b656e6a69@gmail.com>
2022-02-27 16:28:41 +0900

    UnusedTypeParams

---
+++ output/src/main/scala/fix/UnusedTypeParamsTest.scala
@@ -0,0 +1,11 @@
+package fix
+
+import scala.reflect.ClassTag
+
+object UnusedTypeParamsTest {
+  def a1[A](a: Int) = a + 1 
+
+  def a2[A](a: Int)(implicit c: ClassTag[A]) = a + 1
+
+  def a3[A: ClassTag](a: Int) = a + 1
+}
---
+++ rules/src/main/scala/fix/UnusedTypeParams.scala
@@ -0,0 +1,42 @@
+package fix
+
+import scala.meta.Defn
+import scala.meta.Mod
+import scala.meta.Position
+import scala.meta.Term
+import scala.meta.Type
+import scalafix.Diagnostic
+import scalafix.Patch
+import scalafix.lint.LintSeverity
+import scalafix.v1.SyntacticDocument
+import scalafix.v1.SyntacticRule
+
+class UnusedTypeParams extends SyntacticRule("UnusedTypeParams") {
+
+  override def fix(implicit doc: SyntacticDocument): Patch = {
+    doc.tree.collect { case t: Defn.Def =>
+      val typeParams = t.tparams.filter(p => p.cbounds.isEmpty && p.vbounds.isEmpty)
+      val typeParamsMap = typeParams.map(t => t.name.value -> t).toMap
+      val names = t.collect {
+        case x: Term.Name =>
+          typeParamsMap.get(x.value)
+        case x: Type.Name =>
+          typeParamsMap.get(x.value)
+        case x: scala.meta.Name =>
+          typeParamsMap.get(x.value)
+      }.flatten
+      names
+        .groupBy(_.name.value)
+        .values
+        .collect { case a :: Nil =>
+          Patch.lint(new UnusedTypeParamsWarn(a.pos))
+        }
+        .asPatch
+    }.asPatch
+  }
+}
+
+class UnusedTypeParamsWarn(override val position: Position) extends Diagnostic {
+  override def message = "maybe unused type param"
+  override def severity = LintSeverity.Warning
+}
