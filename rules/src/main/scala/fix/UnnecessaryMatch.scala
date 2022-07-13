52d809cf024091a91cd9f69ec57366245b01b486
xuwei-k <6b656e6a69@gmail.com>
2022-07-13 11:46:42 +0900

    UnnecessaryMatch param 2
    [skip ci]

--- rules/src/main/scala/fix/UnnecessaryMatch.scala
+++ rules/src/main/scala/fix/UnnecessaryMatch.scala
@@ -12,23 +12,45 @@ class UnnecessaryMatch extends SyntacticRule("UnnecessaryMatch") {
             _,
             List(
               f @ Term.Function(
-                Term.Param(_, Term.Name(p1), _, _) :: Nil,
-                Term.Match(Term.Name(p2), cases)
+                Term.Param(_, Term.Name(p0), _, _) ::
+                Term.Param(_, Term.Name(p1), _, _) ::
+                Nil,
+                Term.Match(
+                  Term.Tuple(
+                    Term.Name(x1) ::
+                    Term.Name(x2) ::
+                    Nil
+                  ),
+                  cases
+                )
               )
             )
-          ) if p1 == p2 && cases.forall(_.collect { case Term.Name(n) if n == p2 => () }.isEmpty) =>
+          ) if p0 == x1 && p1 == x2 && cases.forall(_.collect {
+            case Term.Name(n) if n == x1 || n == x2 => ()
+          }.isEmpty) =>
         Patch.replaceTree(f, Term.PartialFunction(cases).toString)
       case Term.Apply(
             _,
             Term.Block(
               List(
                 f @ Term.Function(
-                  Term.Param(_, Term.Name(p1), _, _) :: Nil,
-                  Term.Match(Term.Name(p2), cases)
+                  Term.Param(_, Term.Name(p0), _, _) ::
+                  Term.Param(_, Term.Name(p1), _, _) ::
+                  Nil,
+                  Term.Match(
+                    Term.Tuple(
+                      Term.Name(x1) ::
+                      Term.Name(x2) ::
+                      Nil
+                    ),
+                    cases
+                  )
                 )
               )
             ) :: Nil
-          ) if p1 == p2 && cases.forall(_.collect { case Term.Name(n) if n == p2 => () }.isEmpty) =>
+          ) if p0 == x1 && p1 == x2 && cases.forall(_.collect {
+            case Term.Name(n) if n == x1 || n == x2 => ()
+          }.isEmpty) =>
         Patch.replaceTree(f, Term.PartialFunction(cases).toString)
     }
   }.asPatch
