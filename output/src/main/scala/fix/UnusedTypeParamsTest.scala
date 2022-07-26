d9165c5cd8c1cc9e10f2fa94ac1c778d95e03962
xuwei-k <6b656e6a69@gmail.com>
2022-07-26 15:21:08 +0900

    Don't report UnusedTypeParams if `override def`

--- output/src/main/scala/fix/UnusedTypeParamsTest.scala
+++ output/src/main/scala/fix/UnusedTypeParamsTest.scala
@@ -8,4 +8,12 @@ object UnusedTypeParamsTest {
   def a2[A](a: Int)(implicit c: ClassTag[A]) = a + 1
 
   def a3[A: ClassTag](a: Int) = a + 1
+
+  trait A1[F[_]] {
+    def x[B]: F[B]
+  }
+
+  class A2 extends A1[List] {
+    override def x[B] = Nil
+  }
 }
