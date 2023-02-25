fafdb3a16dd00294899fd4f8c904e1fcaf8aa611
xuwei-k <6b656e6a69@gmail.com>
2023-02-25 11:13:46 +0900

    KeySet

---
+++ input/src/main/scala/fix/KeySetTest.scala
@@ -0,0 +1,21 @@
+/*
+rule = KeySet
+ */
+package fix
+
+object KeySetTest {
+  def f[A, B](x: collection.Map[A, B]): collection.Set[A] = x.map(_._1).toSet
+  def f[A, B](x: collection.immutable.Map[A, B]): Set[A] = x.map(_._1).toSet
+  def f[A, B](x: collection.immutable.HashMap[A, B]): Set[A] = x.map(_._1).toSet
+  def f[A, B](x: collection.immutable.TreeMap[A, B]): Set[A] = x.map(_._1).toSet
+  def f[A, B](x: collection.immutable.SortedMap[A, B]): Set[A] = x.map(_._1).toSet
+  def f[A, B](x: collection.mutable.Map[A, B]): collection.Set[A] = x.map(_._1).toSet
+  def f[A, B](x: collection.mutable.HashMap[A, B]): collection.Set[A] = x.map(_._1).toSet
+  def f[A, B](x: collection.mutable.TreeMap[A, B]): collection.Set[A] = x.map(_._1).toSet
+  def f[A, B](x: collection.mutable.SortedMap[A, B]): collection.Set[A] = x.map(_._1).toSet
+  def f[A <: AnyRef, B](x: collection.mutable.AnyRefMap[A, B]): collection.Set[A] = x.map(_._1).toSet
+  def f[A, B](x: collection.concurrent.TrieMap[A, B]): collection.Set[A] = x.map(_._1).toSet
+  def f[A](x: collection.immutable.IntMap[A]): Set[Int] = x.map(_._1).toSet
+  def f[A](x: collection.immutable.LongMap[A]): Set[Long] = x.map(_._1).toSet
+  def f[A, B](x: Seq[(A, B)]): Set[A] = x.map(_._1).toSet
+}
---
+++ rules/src/main/scala/fix/KeySet.scala
@@ -0,0 +1,56 @@
+package fix
+
+import scalafix.Patch
+import scalafix.v1.SemanticDocument
+import scalafix.v1.SemanticRule
+import scalafix.v1.TypeRef
+import scalafix.v1.ValueSignature
+import scalafix.v1.XtensionTreeScalafix
+import scala.meta.Term
+
+class KeySet extends SemanticRule("KeySet") {
+  private[this] val map2Types: Set[String] = Set[Class[?]](
+    classOf[scala.collection.Map[?, ?]],
+    classOf[scala.collection.immutable.Map[?, ?]],
+    classOf[scala.collection.immutable.HashMap[?, ?]],
+    classOf[scala.collection.immutable.TreeMap[?, ?]],
+    classOf[scala.collection.immutable.SortedMap[?, ?]],
+    classOf[scala.collection.mutable.Map[?, ?]],
+    classOf[scala.collection.mutable.HashMap[?, ?]],
+    classOf[scala.collection.mutable.TreeMap[?, ?]],
+    classOf[scala.collection.mutable.SortedMap[?, ?]],
+    classOf[scala.collection.mutable.AnyRefMap[?, ?]],
+    classOf[scala.collection.concurrent.TrieMap[?, ?]],
+  ).map(_.getName + ".")
+
+  private[this] val map1Types: Set[String] = Set[Class[?]](
+    classOf[scala.collection.immutable.IntMap[?]],
+    classOf[scala.collection.immutable.LongMap[?]],
+  ).map(_.getName + ".")
+
+  override def fix(implicit doc: SemanticDocument): Patch = {
+    doc.tree.collect {
+      case t @ Term.Select(
+            Term.Apply(
+              Term.Select(map, Term.Name("map")),
+              List(
+                Term.AnonymousFunction(
+                  Term.Select(Term.Placeholder(), Term.Name("_1"))
+                )
+              )
+            ),
+            Term.Name("toSet")
+          ) =>
+        val p =
+          Patch.replaceTree(t, s"${map}.keySet")
+        map.symbol.info.flatMap { i =>
+          PartialFunction.condOpt(i.signature) {
+            case ValueSignature(TypeRef(_, sym, _ :: _ :: Nil)) if map2Types.contains(sym.normalized.value) =>
+              p
+            case ValueSignature(TypeRef(_, sym, _ :: Nil)) if map1Types.contains(sym.normalized.value) =>
+              p
+          }
+        }.asPatch
+    }.asPatch
+  }
+}
