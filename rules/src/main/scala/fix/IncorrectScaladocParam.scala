9c3a96e58de23674617fb635157bb745ed365502
xuwei-k <6b656e6a69@gmail.com>
2023-04-15 09:00:34 +0900

    IncorrectScaladocParam

---
+++ rules/src/main/scala/fix/IncorrectScaladocParam.scala
@@ -0,0 +1,75 @@
+package fix
+
+import scala.meta.Decl
+import scalafix.Patch
+import scalafix.v1.SyntacticDocument
+import scalafix.v1.SyntacticRule
+import scala.meta.Defn
+import scala.meta.Term
+import scala.meta.Tree
+import scala.meta.contrib.DocToken
+import scala.meta.contrib.XtensionCommentOps
+import scala.meta.inputs.Position
+import scalafix.lint.Diagnostic
+import scalafix.lint.LintSeverity
+
+class IncorrectScaladocParam extends SyntacticRule("IncorrectScaladocParam") {
+  override def fix(implicit doc: SyntacticDocument): Patch = {
+    doc.tree.collect {
+      case t: Defn.Class =>
+        p(t, t.ctor.paramss)
+      case t: Defn.Def =>
+        p(t, t.paramss)
+      case t: Decl.Def =>
+        p(t, t.paramss)
+    }.asPatch
+  }
+
+  private def p(t: Tree, paramss: List[List[Term.Param]])(implicit doc: SyntacticDocument): Patch = {
+    val names = paramss.flatMap(_.map(_.name.value.trim)).toSet
+
+    doc.comments
+      .leading(t)
+      .map { x =>
+        x.docTokens.toList.flatten.map { c =>
+          PartialFunction
+            .condOpt(c.kind) { case DocToken.Param =>
+              c.name
+            }
+            .flatten
+            .map(_.trim)
+            .map { paramName =>
+              if (names(paramName)) {
+                Patch.empty
+              } else {
+                PartialFunction
+                  .condOpt(x.value.linesIterator.zipWithIndex.collect {
+                    case (str, i) if str.contains(s" ${paramName} ") => (str.length, i)
+                  }.toList) { case List((len, index)) =>
+                    Patch.lint(
+                      Diagnostic(
+                        id = "",
+                        message = "incorrect @param",
+                        position = {
+                          val line = x.pos.startLine + index
+                          Position.Range(
+                            input = doc.input,
+                            startLine = line,
+                            startColumn = 0,
+                            endLine = line,
+                            endColumn = len
+                          )
+                        },
+                        severity = LintSeverity.Warning
+                      )
+                    )
+                  }
+                  .asPatch
+              }
+            }
+            .asPatch
+        }.asPatch
+      }
+      .asPatch
+  }
+}
