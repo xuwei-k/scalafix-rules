df858113a6020976a5b06988831082a7bc12d909
xuwei-k <6b656e6a69@gmail.com>
2023-04-15 09:00:43 +0900

    UselessParamCommentsWarn

---
+++ rules/src/main/scala/fix/UselessParamCommentsWarn.scala
@@ -0,0 +1,55 @@
+package fix
+
+import java.util.Locale
+import scalafix.Patch
+import scalafix.lint.Diagnostic
+import scalafix.lint.LintSeverity
+import scalafix.v1.SyntacticDocument
+import scalafix.v1.SyntacticRule
+import scala.meta.Defn
+import scala.meta.inputs.Position
+import scala.meta.contrib.DocToken
+import scala.meta.contrib.XtensionCommentOps
+
+class UselessParamCommentsWarn extends SyntacticRule("UselessParamCommentsWarn") {
+  override def fix(implicit doc: SyntacticDocument): Patch = {
+    doc.tree.collect { case t: Defn.Class =>
+      doc.comments
+        .leading(t)
+        .flatMap(x => x.docTokens.toList.flatten.map(x -> _))
+        .map { case (x, c) =>
+          PartialFunction
+            .condOpt(
+              (c.kind, c.name.map(_.toLowerCase(Locale.ROOT)), c.body.map(_.toLowerCase(Locale.ROOT)))
+            ) {
+              case (DocToken.Param, Some(x1), Some(x2)) if x1 == x2 =>
+                PartialFunction
+                  .condOpt(x.value.linesIterator.zipWithIndex.collect {
+                    case (str, i) if str.contains(s" ${x1} ") => (str.length + 1, i)
+                  }.toList) { case List((length, index)) =>
+                    Patch.lint(
+                      Diagnostic(
+                        id = "",
+                        message = "useless @param",
+                        position = {
+                          val line = x.pos.startLine + index
+                          Position.Range(
+                            input = doc.input,
+                            startLine = line,
+                            startColumn = 0,
+                            endLine = line,
+                            endColumn = length
+                          )
+                        },
+                        severity = LintSeverity.Warning
+                      )
+                    )
+                  }
+                  .asPatch
+            }
+            .asPatch
+        }
+        .asPatch
+    }.asPatch
+  }
+}
