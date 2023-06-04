047e74e24746a1fae4b23289d2309b3566301343
xuwei-k <6b656e6a69@gmail.com>
2023-06-04 09:32:47 +0900

    remove redundant toSet

--- rules/src/main/scala/fix/UnusedConstructorParams.scala
+++ rules/src/main/scala/fix/UnusedConstructorParams.scala
@@ -22,7 +22,7 @@ class UnusedConstructorParams extends SyntacticRule("UnusedConstructorParams") {
         val allTokens = {
           val values = x.templ.tokens.map(_.text).toSet
           values ++ values.filter(a => a.startsWith("`") && a.endsWith("`")).map(_.drop(1).dropRight(1))
-        }.toSet
+        }
         val maybeUnused = params.filterNot(p => allTokens(p.name.value))
         maybeUnused.map { a =>
           Patch.lint(
