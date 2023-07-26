2ad532ee37372da7073796e33991513b8e08c85f
mackenziestarr <mackestarr@gmail.com>
2023-07-25 23:07:29 -0400

    MapSequenceTraverse: don't add redundant parentheses when original source uses braces

--- input/src/main/scala/fix/MapSequenceTraverseTest.scala
+++ input/src/main/scala/fix/MapSequenceTraverseTest.scala
@@ -6,5 +6,8 @@ package fix
 import cats.syntax.all.*
 
 object MapSequenceTraverseTest {
-  def x: Option[List[Int]] = List(1, 2, 3).map(a => if (a % 2 == 0) Some(a) else None).sequence
+  def withParens: Option[List[Int]] = List(1, 2, 3).map(a => if (a % 2 == 0) Some(a) else None).sequence
+  def withBraces: Option[List[Int]] = List(1, 2, 3).map{a => if (a % 2 == 0) Some(a) else None}.sequence
+  def withParensThenBraces: Option[List[Int]] = List(1, 2, 3).map({a => if (a % 2 == 0) Some(a) else None}).sequence
+  def withBracesThenParens: Option[List[Int]] = List(1, 2, 3).map{(a => if (a % 2 == 0) Some(a) else None)}.sequence
 }
--- output/src/main/scala/fix/MapSequenceTraverseTest.scala
+++ output/src/main/scala/fix/MapSequenceTraverseTest.scala
@@ -3,5 +3,8 @@ package fix
 import cats.syntax.all.*
 
 object MapSequenceTraverseTest {
-  def x: Option[List[Int]] = List(1, 2, 3).traverse(a => if (a % 2 == 0) Some(a) else None)
+  def withParens: Option[List[Int]] = List(1, 2, 3).traverse(a => if (a % 2 == 0) Some(a) else None)
+  def withBraces: Option[List[Int]] = List(1, 2, 3).traverse{a => if (a % 2 == 0) Some(a) else None}
+  def withParensThenBraces: Option[List[Int]] = List(1, 2, 3).traverse{a => if (a % 2 == 0) Some(a) else None}
+  def withBracesThenParens: Option[List[Int]] = List(1, 2, 3).traverse{(a => if (a % 2 == 0) Some(a) else None)}
 }
