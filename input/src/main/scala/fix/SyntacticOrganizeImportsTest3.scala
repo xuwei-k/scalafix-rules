7ff5e2ea655a5b2a73b8ccdc5d954aceefe9c965
xuwei-k <6b656e6a69@gmail.com>
2024-02-02 19:02:55 +0900

    support Rename in SyntacticOrganizeImports

---
+++ input/src/main/scala/fix/SyntacticOrganizeImportsTest3.scala
@@ -0,0 +1,9 @@
+/*
+rule = SyntacticOrganizeImports
+ */
+package fix
+
+import scala.collection.mutable.ArrayBuffer as A // assert: SyntacticOrganizeImports
+import scala.collection.mutable.SortedMap
+
+class SyntacticOrganizeImportsTest3
---
+++ sbt-test/SyntacticOrganizeImports/test-1/A.scala
@@ -0,0 +1,10 @@
+package foo
+
+import scala.util.Random
+
+import scala.util.Success
+
+
+import scala.util.Try
+
+trait A
