e595e2b9effa61f226d97916282854fb42b51a61
xuwei-k <6b656e6a69@gmail.com>
2024-02-07 14:34:20 +0900

    fix UnnecessaryMatch

--- output/src/main/scala/fix/UnnecessaryMatchTest.scala
+++ output/src/main/scala/fix/UnnecessaryMatchTest.scala
@@ -1,16 +1,16 @@
 package fix
 
 class UnnecessaryMatchTest {
-  List(1).map { {
-  case 2 => 3
-  case 4 => 5
-}
+  List(1).map {  {
+      case 2 => 3
+      case 4 => 5 // comment 1
+    }
   }
 
-  List("a1").map({
-  case "a2" => 7
-  case "a3" => 8
-}
+  List("a1").map( {
+      case "a2" => 7 // comment 2
+      case "a3" => 8
+    }
   )
 
   List(1).map { a =>
