17c8fb3c64f67588f7f3af4bbe1347b6aeb15611
xuwei-k <6b656e6a69@gmail.com>
2024-03-03 09:40:50 +0900

    fix SyntacticOrganizeImports

--- input/src/main/scala/fix/SyntacticOrganizeImportsTest3.scala
+++ input/src/main/scala/fix/SyntacticOrganizeImportsTest3.scala
@@ -3,7 +3,7 @@ rule = SyntacticOrganizeImports
  */
 package fix
 
-import scala.collection.mutable.ArrayBuffer as A // assert: SyntacticOrganizeImports
+import scala.collection.mutable.ArrayBuffer as A
 import scala.collection.mutable.SortedMap
 
 class SyntacticOrganizeImportsTest3
