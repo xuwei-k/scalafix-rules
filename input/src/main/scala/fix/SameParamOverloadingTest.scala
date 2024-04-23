1bd75a43fc056d1c5582cf0070a146f9123dd6bb
xuwei-k <6b656e6a69@gmail.com>
2024-04-24 07:45:31 +0900

    ignore type params in SameParamOverloading

--- input/src/main/scala/fix/SameParamOverloadingTest.scala
+++ input/src/main/scala/fix/SameParamOverloadingTest.scala
@@ -3,6 +3,7 @@ rule = SameParamOverloading
  */
 package fix
 
+import scala.concurrent.ExecutionContext
 import scala.reflect.ClassTag
 
 object SameParamOverloadingTest {
@@ -10,6 +11,18 @@ object SameParamOverloadingTest {
   abstract class A1 {
     def x1[A: ClassTag](a: A): Int // assert: SameParamOverloading
     def x1[A](b: A): String // assert: SameParamOverloading
+
+    def x2[A: ClassTag](a: Either[Int, A]): Int // assert: SameParamOverloading
+    def x2(b: Either[Int, Int]): String // assert: SameParamOverloading
+    def x2(c: Boolean Either Long)(implicit e: ExecutionContext): Boolean // assert: SameParamOverloading
+
+    def func[A: ClassTag](a: Int => A): Int // assert: SameParamOverloading
+    def func(b: String => Boolean): String // assert: SameParamOverloading
+    def func(c: (String, Long) => Boolean): String
+
+    def tuple[A: ClassTag](a: (Int, A)): Int // assert: SameParamOverloading
+    def tuple(b: (Long, Boolean)): String // assert: SameParamOverloading
+    def tuple(c: (Int, Char, Long)): String
   }
 
   trait A2 {
