a8009869749ab60f80e7ee645266f93166700ebe
xuwei-k <6b656e6a69@gmail.com>
2024-06-25 20:24:47 +0900

    テスト追加

--- input/src/main/scala/fix/ApplyInfixNoParenTest.scala
+++ input/src/main/scala/fix/ApplyInfixNoParenTest.scala
@@ -3,4 +3,8 @@ rule = ApplyInfixNoParen
  */
 package fix
 
-trait ApplyInfixNoParenTest
+trait ApplyInfixNoParenTest {
+  def f1: Boolean = false == true && false // assert: ApplyInfixNoParen
+  def f2: Boolean = (false == true) && false // これはOK
+  def f3: Boolean = false == (true && false) // これもOK
+}
