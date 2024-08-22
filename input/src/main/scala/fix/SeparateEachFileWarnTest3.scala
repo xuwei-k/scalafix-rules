62ee3f3c74acb185cf45fa501ffe619466814e34
xuwei-k <6b656e6a69@gmail.com>
2024-08-23 07:36:16 +0900

    fix SeparateEachFile

---
+++ input/src/main/scala/fix/SeparateEachFileWarnTest3.scala
@@ -0,0 +1,8 @@
+/*
+rule = SeparateEachFileWarn
+ */
+package fix
+
+sealed trait SeparateEachFileWarnTest3 extends Product
+case object SeparateEachFileWarnTest3_A extends SeparateEachFileWarnTest3
+case object SeparateEachFileWarnTest3_B extends SeparateEachFileWarnTest3
