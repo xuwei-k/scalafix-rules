6a317ca3da30590adf8d7e56e07ac0010e178f35
xuwei-k <6b656e6a69@gmail.com>
2024-10-30 18:57:17 +0900

    fix UnusedConstructorParams

---
+++ input/src/main/scala-3/fix/UnusedConstructorParamsTest2.scala
@@ -0,0 +1,6 @@
+/*
+rule = UnusedConstructorParams
+ */
+package fix
+
+class A6(val x: Int)(using x2: String)
--- rules/src/main/scala/fix/UnusedConstructorParams.scala
+++ rules/src/main/scala/fix/UnusedConstructorParams.scala
@@ -21,6 +21,7 @@ class UnusedConstructorParams extends SyntacticRule("UnusedConstructorParams") {
       case x: Defn.Class if !x.mods.exists(_.is[Mod.Case]) =>
         val params = x.ctor.paramClauses.flatten
           .filterNot(_.mods.exists(_.is[Mod.Implicit]))
+          .filterNot(_.mods.exists(_.is[Mod.Using]))
           .filterNot(_.mods.exists(_.is[Mod.VarParam]))
           .filterNot(_.mods.exists(_.is[Mod.ValParam]))
         val allTokens = {
