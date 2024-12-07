297d1f03b99930714d4f56790a74b1c9563564f6
xuwei-k <6b656e6a69@gmail.com>
2024-12-07 09:55:32 +0900

    fix CollectHeadOption
    (cherry picked from commit 3dd9cfe0f2c32d2ccf41aeb1955285ee62f803e9)

--- input/src/main/scala/fix/CollectHeadOptionTest.scala
+++ input/src/main/scala/fix/CollectHeadOptionTest.scala
@@ -5,4 +5,5 @@ package fix
 
 object CollectHeadOptionTest {
   def x1: Option[String] = List(1, 2, 3).collect { case n if n % 2 == 0 => n.toString }.headOption
+  def x2(f: PartialFunction[Int, String]): Option[String] = List(1, 2, 3).collect(f).headOption
 }
