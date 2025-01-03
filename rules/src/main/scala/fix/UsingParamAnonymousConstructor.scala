bcf990cf7bb4c63fd518d989a2a664bc18285bef
xuwei-k <6b656e6a69@gmail.com>
2025-01-03 15:19:38 +0900

    UsingParamAnonymousConstructor
    (cherry picked from commit 04ab3aceaa4c3aa8c5178c40cb34f075a5935067)

---
+++ rules/src/main/scala/fix/UsingParamAnonymousConstructor.scala
@@ -0,0 +1,41 @@
+package fix
+
+import scala.meta.Mod
+import scala.meta.Stat
+import scala.meta.Term
+import scala.meta.XtensionClassifiable
+import scala.meta.XtensionCollectionLikeUI
+import scala.meta.tokens.Token
+import scalafix.Patch
+import scalafix.v1.SyntacticDocument
+import scalafix.v1.SyntacticRule
+import scalafix.v1.XtensionSeqPatch
+
+class UsingParamAnonymousConstructor extends SyntacticRule("UsingParamAnonymousConstructor") {
+  override def fix(implicit doc: SyntacticDocument): Patch = {
+    doc.tree.collect { case c: Stat.WithCtor =>
+      val t = c.ctor
+      t.paramClauses.filter { paramClause =>
+        paramClause.values.forall(_.mods.exists(_.is[Mod.Using])) && paramClause.values.forall(m =>
+          !m.mods.exists(_.is[Mod.Inline]) && !m.mods.exists(_.is[Mod.Annot])
+        )
+      }.map { paramClause =>
+        val countOneNames = c.collect { case x: Term.Name =>
+          x.value
+        }.groupBy(identity).filter(_._2.size == 1).keySet
+        if (paramClause.values.forall(a => countOneNames(a.name.value))) {
+          paramClause.values
+            .map(p =>
+              Seq(
+                Patch.removeTokens(p.name.tokens),
+                Patch.removeToken(p.tokens.find(_.is[Token.Colon]).get)
+              ).asPatch
+            )
+            .asPatch
+        } else {
+          Patch.empty
+        }
+      }.asPatch
+    }.asPatch
+  }
+}
