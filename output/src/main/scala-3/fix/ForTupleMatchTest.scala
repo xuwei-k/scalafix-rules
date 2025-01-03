a7af0dca0393fdbdc29ff2ecf679b77608fae346
xuwei-k <6b656e6a69@gmail.com>
2025-01-03 14:53:47 +0900

    ForTupleMatch
    (cherry picked from commit 8358702d1fe21bce080d1b5669200c77754ee89c)

---
+++ output/src/main/scala-3/fix/ForTupleMatchTest.scala
@@ -0,0 +1,35 @@
+package fix
+
+trait ForTupleMatchTest {
+  def x1: List[(Int, String)]
+  def x2: List[(Int, String, Boolean)]
+
+  def f0: List[(String, Int)] = for {
+    (b, c, _) <- x2
+    
+  } yield (c, b)
+
+  def f1: List[(String, Int)] = for {
+    (b, c) <- x1
+    
+  } yield (c, b)
+
+  def f2: List[((Int, String), Int)] = for {
+    a <- x1
+    (b, c) = a
+  } yield (a, b)
+
+  def f3: Unit = for {
+    (b, c) <- x1
+    
+  } {
+    println(b)
+  }
+
+  def f4: Unit = for {
+    a <- x1
+    (b, c) = a
+  } {
+    println(a._2)
+  }
+}
