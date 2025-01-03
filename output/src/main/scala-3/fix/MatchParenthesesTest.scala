d10c78220cdddc5d21a36da1a2fd2bfae7adb206
xuwei-k <6b656e6a69@gmail.com>
2025-01-03 14:37:32 +0900

    MatchParentheses

---
+++ output/src/main/scala-3/fix/MatchParenthesesTest.scala
@@ -0,0 +1,15 @@
+package fix
+
+trait MatchParenthesesTest {
+  def f1(x: List[Int]): Int = x .match {
+    case Nil => ","
+    case _ => x.mkString(",")
+  }.length
+
+  def g(s: String): String
+
+  def f2(x: List[Int]): Int = g(x match {
+    case Nil => ","
+    case _ => x.mkString(",")
+  }).length
+}
