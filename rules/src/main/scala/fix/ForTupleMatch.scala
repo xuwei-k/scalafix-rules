a3ebda59381ad6a47f8c504c16219a1ed121ae13
xuwei-k <6b656e6a69@gmail.com>
2025-01-03 14:53:47 +0900

    ForTupleMatch

---
+++ rules/src/main/scala/fix/ForTupleMatch.scala
@@ -0,0 +1,33 @@
+package fix
+
+import scala.meta.Enumerator
+import scala.meta.Pat
+import scala.meta.Term
+import scala.meta.XtensionCollectionLikeUI
+import scalafix.Patch
+import scalafix.v1.SyntacticDocument
+import scalafix.v1.SyntacticRule
+import scalafix.v1.XtensionSeqPatch
+
+class ForTupleMatch extends SyntacticRule("ForTupleMatch") {
+  override def fix(implicit doc: SyntacticDocument): Patch = {
+    doc.tree.collect { case t: Term.ForClause =>
+      t.enums
+        .zip(t.enums.drop(1))
+        .collect {
+          case (
+                Enumerator.Generator(Pat.Var(tuple1: Term.Name), _),
+                tupleVal @ Enumerator.Val(
+                  extracted: Pat.Tuple,
+                  Term.Name(tuple2)
+                )
+              ) if tuple1.value == tuple2 && (t.collect { case Term.Name(x) if x == tuple1.value => () }.size == 2) =>
+            Seq(
+              Patch.replaceTree(tuple1, extracted.toString),
+              Patch.removeTokens(tupleVal.tokens)
+            ).asPatch
+        }
+        .asPatch
+    }.asPatch
+  }
+}
