85cef3f8f6ae9be7e976d61d65e0350b79040893
xuwei-k <6b656e6a69@gmail.com>
2025-01-03 13:08:05 +0900

    UsingParamAnonymous
    (cherry picked from commit 0c0e8616b075bec7aba797b44fcdb2bed9f8d6b8)

---
+++ input/src/main/scala-3/fix/UsingParamAnonymousTest.scala
@@ -0,0 +1,9 @@
+/*
+rule = UsingParamAnonymous
+ */
+package fix
+
+trait UsingParamAnonymousTest {
+  def f1(using a1: Int, a2: Int): Int = List(a1, 2).sum
+  def f2(using a1: Int, a2: String): Int = summon[Int] + summon[String].size
+}
