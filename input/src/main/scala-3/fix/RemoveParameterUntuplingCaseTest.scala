a5aca1884091582e2d66fd896a96d248bf56d422
xuwei-k <6b656e6a69@gmail.com>
2025-02-18 08:27:50 +0900

    RemoveParameterUntuplingCase

---
+++ input/src/main/scala-3/fix/RemoveParameterUntuplingCaseTest.scala
@@ -0,0 +1,20 @@
+/*
+rule = RemoveParameterUntuplingCase
+ */
+package fix
+
+trait RemoveParameterUntuplingCaseTest {
+  def xs: List[(Int, Int)]
+
+  final val C: Int = 3
+
+  val D: PartialFunction[Int, Int] = { case x => x }
+
+  xs.map { case (a, b) => a + b }
+
+  xs.map { case (a, 2) => a }
+
+  xs.map { case (a, C) => a }
+
+  xs.map { case (D(a), b) => a + b }
+}
