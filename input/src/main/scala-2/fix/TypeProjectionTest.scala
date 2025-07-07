3baa6710e1e1811bd85af634a06c4cdcbb7cc44a
xuwei-k <6b656e6a69@gmail.com>
2025-07-07 17:01:11 +0900

    TypeProjection: don't report type lambda

--- input/src/main/scala-2/fix/TypeProjectionTest.scala
+++ input/src/main/scala-2/fix/TypeProjectionTest.scala
@@ -7,4 +7,8 @@ trait TypeProjectionTest {
   type A
 
   def f1: TypeProjectionTest#A // assert: TypeProjection
+
+  trait Functor[F[_]]
+
+  type X = Functor[({ type l[x] = Either[String, x] })#l]
 }
