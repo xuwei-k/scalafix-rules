76b07bf28a941bfcce1e94278cb7dee73697c4b4
xuwei-k <6b656e6a69@gmail.com>
2025-08-18 13:39:18 +0900

    fix UsingParamAnonymous

--- output/src/main/scala-3/fix/UsingParamAnonymousTest.scala
+++ output/src/main/scala-3/fix/UsingParamAnonymousTest.scala
@@ -4,4 +4,5 @@ trait UsingParamAnonymousTest {
   def f1(using a1: Int, a2: Int): Int = List(a1, 2).sum
   def f2(using  Int,  String): Int = summon[Int] + summon[String].size
   def f3(using @annotation.unused a1: Int): Int = summon[Int]
+  def f4(using a1: Int, a2: Int = 3, a3: Int): Int = 9
 }
