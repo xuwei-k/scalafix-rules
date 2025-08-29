e7056ac1492b6bab0d426a9d0993a7e304d117a5
xuwei-k <6b656e6a69@gmail.com>
2025-08-29 12:13:35 +0900

    support Defn.GivenAlias in UsingParamAnonymous

--- input/src/main/scala-3/fix/UsingParamAnonymousTest.scala
+++ input/src/main/scala-3/fix/UsingParamAnonymousTest.scala
@@ -8,4 +8,6 @@ trait UsingParamAnonymousTest {
   def f2(using a1: Int, a2: String): Int = summon[Int] + summon[String].size
   def f3(using @annotation.unused a1: Int): Int = summon[Int]
   def f4(using a1: Int, a2: Int = 3, a3: Int): Int = 9
+
+  given (using a1: Int): Int = 4
 }
--- rules/src/main/scala/fix/UsingParamAnonymous.scala
+++ rules/src/main/scala/fix/UsingParamAnonymous.scala
@@ -3,6 +3,7 @@ package fix
 import scala.meta.Defn
 import scala.meta.Mod
 import scala.meta.Term
+import scala.meta.Tree.WithParamClauses
 import scala.meta.XtensionClassifiable
 import scala.meta.XtensionCollectionLikeUI
 import scala.meta.tokens.Token
@@ -13,30 +14,31 @@ import scalafix.v1.XtensionSeqPatch
 
 class UsingParamAnonymous extends SyntacticRule("UsingParamAnonymous") {
   override def fix(implicit doc: SyntacticDocument): Patch = {
-    doc.tree.collect { case t: Defn.Def =>
-      t.paramClauses.filter { paramClause =>
-        paramClause.values.forall(
-          _.mods.exists(_.is[Mod.Using])
-        ) && paramClause.values.forall(
-          !_.mods.exists(x => x.is[Mod.Inline] || x.is[Mod.Annot])
-        ) && paramClause.values.forall(_.default.isEmpty)
-      }.map { paramClause =>
-        val countOneNames = t.collect { case x: Term.Name =>
-          x.value
-        }.groupBy(identity).filter(_._2.size == 1).keySet
-        if (paramClause.values.forall(a => countOneNames(a.name.value))) {
-          paramClause.values
-            .map(p =>
-              Seq(
-                Patch.removeTokens(p.name.tokens),
-                Patch.removeToken(p.tokens.find(_.is[Token.Colon]).get)
-              ).asPatch
-            )
-            .asPatch
-        } else {
-          Patch.empty
-        }
-      }.asPatch
+    doc.tree.collect {
+      case t: WithParamClauses if t.is[Defn.Def] || t.is[Defn.GivenAlias] =>
+        t.paramClauses.filter { paramClause =>
+          paramClause.values.forall(
+            _.mods.exists(_.is[Mod.Using])
+          ) && paramClause.values.forall(
+            !_.mods.exists(x => x.is[Mod.Inline] || x.is[Mod.Annot])
+          ) && paramClause.values.forall(_.default.isEmpty)
+        }.map { paramClause =>
+          val countOneNames = t.collect { case x: Term.Name =>
+            x.value
+          }.groupBy(identity).filter(_._2.size == 1).keySet
+          if (paramClause.values.forall(a => countOneNames(a.name.value))) {
+            paramClause.values
+              .map(p =>
+                Seq(
+                  Patch.removeTokens(p.name.tokens),
+                  Patch.removeToken(p.tokens.find(_.is[Token.Colon]).get)
+                ).asPatch
+              )
+              .asPatch
+          } else {
+            Patch.empty
+          }
+        }.asPatch
     }.asPatch
   }
 }
