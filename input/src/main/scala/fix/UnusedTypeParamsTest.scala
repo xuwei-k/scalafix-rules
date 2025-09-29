1171fe7445ad70c6f2a74206c6aa6b3a5ec9e40d
xuwei-k <6b656e6a69@gmail.com>
2025-09-30 07:32:03 +0900

    support unused annotation in UnusedTypeParams

--- input/src/main/scala/fix/UnusedTypeParamsTest.scala
+++ input/src/main/scala/fix/UnusedTypeParamsTest.scala
@@ -3,6 +3,7 @@ rule = UnusedTypeParams
  */
 package fix
 
+import scala.annotation.unused
 import scala.reflect.ClassTag
 
 object UnusedTypeParamsTest {
@@ -11,6 +12,14 @@ object UnusedTypeParamsTest {
   def a2[A](a: Int)(implicit c: ClassTag[A]) = a + 1
 
   def a3[A: ClassTag](a: Int) = a + 1
+
+  def a4[@unused A](a: Int) = a
+
+  def a5[@annotation.unused A](a: Int) = a
+
+  def a6[@scala.annotation.unused A](a: Int) = a
+
+  def a7[@ _root_.scala.annotation.unused A](a: Int) = a
 
   trait A1[F[_]] {
     def x[B]: F[B]
