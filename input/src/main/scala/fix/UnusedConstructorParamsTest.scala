2ed3c0398e99a665510e338fa01aa2b35d188d0b
xuwei-k <6b656e6a69@gmail.com>
2025-09-30 07:22:33 +0900

    support unused annotation in UnusedConstructorParams

--- input/src/main/scala/fix/UnusedConstructorParamsTest.scala
+++ input/src/main/scala/fix/UnusedConstructorParamsTest.scala
@@ -1,14 +1,32 @@
-package fix
-
 /*
 rule = UnusedConstructorParams
  */
-class A1(val x: Int)
+package fix
 
-class A2(x: Int) // assert: UnusedConstructorParams
+import scala.annotation.unused
 
-class A3(`type`: Int) extends A1(`type`)
+object UnusedConstructorParamsTest {
+  class A1(val x: Int)
 
-class A4(var x: Int)
+  class A2(x: Int) // assert: UnusedConstructorParams
 
-class A5(val x: Int)(implicit x2: String)
+  class A3(`type`: Int) extends A1(`type`)
+
+  class A4(var x: Int)
+
+  class A5(val x: Int)(implicit x2: String)
+
+  class A6(@unused x: Int)
+
+  class A7(@annotation.unused x: Int)
+
+  class A8(@scala.annotation.unused x: Int)
+
+  class A9(@ _root_.scala.annotation.unused x: Int)
+
+  class A10(
+    @unused x1: Int,
+    x2: Int, // assert: UnusedConstructorParams
+    @unused x3: Int,
+  )
+}
