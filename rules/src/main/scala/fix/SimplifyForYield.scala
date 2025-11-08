b514907528f4559713e237fa38128b64f4aceab3
xuwei-k <6b656e6a69@gmail.com>
2025-11-08 17:46:57 +0900

    Update SimplifyForYield

--- rules/src/main/scala/fix/SimplifyForYield.scala
+++ rules/src/main/scala/fix/SimplifyForYield.scala
@@ -3,18 +3,21 @@ package fix
 import scala.meta.Enumerator.Generator
 import scala.meta.Pat
 import scala.meta.Term
+import scala.meta.Tree
 import scala.meta.XtensionClassifiable
 import scala.meta.XtensionCollectionLikeUI
 import scala.meta.tokens.Token
 import scalafix.patch.Patch
 import scalafix.v1.SyntacticDocument
 import scalafix.v1.SyntacticRule
-import scalafix.v1.XtensionOptionPatch
 import scalafix.v1.XtensionSeqPatch
 
 class SimplifyForYield extends SyntacticRule("SimplifyForYield") {
-  override def fix(implicit doc: SyntacticDocument): Patch = {
-    doc.tree.collect { case x1: Term.ForYield =>
+  override def fix(implicit doc: SyntacticDocument): Patch =
+    getPatch(doc.tree).asPatch
+
+  private def getPatch(tree: Tree): Seq[Patch] = {
+    tree.collect { case x1: Term.ForYield =>
       val generatorAndRhs = PartialFunction
         .condOpt(x1.enumsBlock.enums) { case List(x2: Generator) =>
           PartialFunction.condOpt(x2.pat) { case x3: Pat.Var =>
@@ -26,23 +29,21 @@ class SimplifyForYield extends SyntacticRule("SimplifyForYield") {
         x2.value
       }
 
-      PartialFunction
-        .condOpt((generatorAndRhs, bodyNameOpt)) {
-          case (Some((generator, rhs)), Some(body)) if generator == body =>
-            if (
-              x1.parent.toList
-                .flatMap(_.tokens)
-                .takeWhile(_.pos.start == x1.tokens.head.pos.start)
-                .lastOption
-                .exists(_.is[Token.LeftParen])
-            ) {
-              Patch.replaceTree(x1, s"(${rhs})")
-            } else {
-              Patch.replaceTree(x1, rhs.toString)
-            }
-        }
-        .asPatch
-    }.asPatch
+      PartialFunction.condOpt((generatorAndRhs, bodyNameOpt)) {
+        case (Some((generator, rhs)), Some(body)) if (generator == body) && getPatch(rhs).isEmpty =>
+          if (
+            x1.parent.toList
+              .flatMap(_.tokens)
+              .takeWhile(_.pos.start == x1.tokens.head.pos.start)
+              .lastOption
+              .exists(_.is[Token.LeftParen])
+          ) {
+            Patch.replaceTree(x1, s"(${rhs})")
+          } else {
+            Patch.replaceTree(x1, rhs.toString)
+          }
+      }
+    }.flatten
   }
 
 }
