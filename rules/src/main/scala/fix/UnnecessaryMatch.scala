961735c77ff784088b043bdb1c341984cce2b268
xuwei-k <6b656e6a69@gmail.com>
2025-11-29 08:46:06 +0900

    Update UnnecessaryMatch

--- rules/src/main/scala/fix/UnnecessaryMatch.scala
+++ rules/src/main/scala/fix/UnnecessaryMatch.scala
@@ -12,29 +12,9 @@ import scalafix.v1.XtensionSeqPatch
 class UnnecessaryMatch extends SyntacticRule("UnnecessaryMatch") {
   override def fix(implicit doc: SyntacticDocument): Patch = {
     doc.tree.collect {
-      case Term.Apply.Initial(
-            _,
-            List(
-              f @ Term.Function.Initial(
-                Term.Param(_, Term.Name(p1), _, _) :: Nil,
-                Term.Match.After_4_4_5(Term.Name(p2), cases, _)
-              )
-            )
-          ) if p1 == p2 && cases.forall(_.collect { case Term.Name(n) if n == p2 => () }.isEmpty) =>
-        Seq(
-          Patch.removeTokens(f.tokens.takeWhile(!_.is[Token.KwMatch])),
-          Patch.removeTokens(f.tokens.find(_.is[Token.KwMatch]))
-        ).asPatch
-      case Term.Apply.Initial(
-            _,
-            Term.Block(
-              List(
-                f @ Term.Function.Initial(
-                  Term.Param(_, Term.Name(p1), _, _) :: Nil,
-                  Term.Match.After_4_4_5(Term.Name(p2), cases, _)
-                )
-              )
-            ) :: Nil
+      case f @ Term.Function.After_4_6_0(
+            Term.ParamClause(Term.Param(_, Term.Name(p1), _, _) :: Nil, _),
+            Term.Match.After_4_4_5(Term.Name(p2), cases, _)
           ) if p1 == p2 && cases.forall(_.collect { case Term.Name(n) if n == p2 => () }.isEmpty) =>
         Seq(
           Patch.removeTokens(f.tokens.takeWhile(!_.is[Token.KwMatch])),
