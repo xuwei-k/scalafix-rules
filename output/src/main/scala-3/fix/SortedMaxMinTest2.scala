a4253a85a196637dea072a52869bf3022afdbc15
xuwei-k <6b656e6a69@gmail.com>
2025-11-29 20:11:34 +0900

    Update SortedMaxMin. support sortBy

--- output/src/main/scala-3/fix/SortedMaxMinTest2.scala
+++ output/src/main/scala-3/fix/SortedMaxMinTest2.scala
@@ -3,4 +3,6 @@ package fix
 object SortedMaxMinTest2 {
   def f1(x: List[Int]): Option[Int] = x.minOption
   def f2(x: Vector[Int]): Option[Int] = x.maxOption
+  def f3(x: Seq[Int], y: Int => String): Option[Int] = x.minByOption(y)
+  def f4(x: LazyList[Int], y: Int => String): Option[Int] = x.maxByOption(y)
 }
