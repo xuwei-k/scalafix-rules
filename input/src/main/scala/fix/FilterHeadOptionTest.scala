489754a4236562584bd2a8834611df73afe3dac4
xuwei-k <6b656e6a69@gmail.com>
2025-11-29 15:36:00 +0900

    FilterHeadOption

---
+++ input/src/main/scala/fix/FilterHeadOptionTest.scala
@@ -0,0 +1,12 @@
+/*
+rule = FilterHeadOption
+ */
+package fix
+
+object FilterHeadOptionTest {
+  def f1(x: Seq[Int]): Option[Int] = x.filter(_ == 2).headOption
+  def f2(x: List[String]): Option[String] = x.filter { y => y != "a" }.headOption
+  def f3(x: Set[Int]): Option[Int] = x.filter(_ == 3).headOption
+  def f4(x: Vector[Int]): Option[Int] = x.filter(_ == 4).headOption
+  def f5(x: Map[Int, Int]): Option[(Int, Int)] = x.filter(_._1 == 5).headOption
+}
