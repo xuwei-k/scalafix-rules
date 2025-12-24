d45311a3b3bce25139f752c00ec4b8061022212c
xuwei-k <6b656e6a69@gmail.com>
2025-12-24 14:47:52 +0900

    fix FlatTraverse

--- output/src/main/scala/fix/FlatTraverseTest.scala
+++ output/src/main/scala/fix/FlatTraverseTest.scala
@@ -3,5 +3,12 @@ package fix
 import cats.syntax.all.*
 
 object FlatTraverseTest {
-  def x: Option[List[Int]] = List(1).flatTraverse(a => Option(List(a)))
+  def x1: Option[List[Int]] = List(1).flatTraverse(a => Option(List(a)))
+
+  def x2: Option[List[Int]] = List(1).flatTraverse{ a => Option(List(a)) }
+
+  def x3: Option[List[Int]] = List(1).flatTraverse{ a =>
+    // comment
+    Option(List(a))
+  }
 }
