8101720b8dac0daa1b9d3a3ea5e43f802b23f64b
xuwei-k <6b656e6a69@gmail.com>
2025-12-25 13:08:17 +0900

    ProtectedInObject

---
+++ rules/src/main/scala/fix/ProtectedInObject.scala
@@ -0,0 +1,28 @@
+package fix
+
+import scala.meta.Defn
+import scala.meta.Mod
+import scala.meta.Name
+import scala.meta.Pkg
+import scala.meta.Stat
+import scala.meta.Term
+import scala.meta.XtensionClassifiable
+import scala.meta.XtensionCollectionLikeUI
+import scalafix.Patch
+import scalafix.v1.SyntacticDocument
+import scalafix.v1.SyntacticRule
+import scalafix.v1.XtensionSeqPatch
+
+class ProtectedInObject extends SyntacticRule("ProtectedInObject") {
+  override def fix(implicit doc: SyntacticDocument): Patch = {
+    doc.tree.collect {
+      case t: Defn.Object if (t.parent.exists(_.is[Pkg.Body]) || t.parent.isEmpty) && t.templ.inits.isEmpty =>
+        t.templ.body.stats.collect { case x: Stat.WithMods =>
+          x.mods.collect {
+            case m: Mod.Protected if m.within.is[Term.This] || m.within.is[Name.Anonymous] =>
+              Patch.replaceTree(m, "private")
+          }.asPatch
+        }.asPatch
+    }.asPatch
+  }
+}
