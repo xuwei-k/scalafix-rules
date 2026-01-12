97f10a239ad7b26a158535a944a46bd644b9b86c
xuwei-k <6b656e6a69@gmail.com>
2026-01-12 14:39:33 +0900

    Update ArrayToString. support interpolation

--- rules/src/main/scala/fix/ArrayToString.scala
+++ rules/src/main/scala/fix/ArrayToString.scala
@@ -1,5 +1,8 @@
 package fix
 
+import fix.ArrayToString.BlockLastOrSelf
+import scala.meta.Position
+import scala.meta.Stat
 import scala.meta.Term
 import scala.meta.XtensionCollectionLikeUI
 import scalafix.Patch
@@ -14,36 +17,57 @@ import scalafix.v1.ValueSignature
 import scalafix.v1.XtensionSeqPatch
 import scalafix.v1.XtensionTreeScalafix
 
+private object ArrayToString {
+  private object BlockLastOrSelf {
+    def unapply(t: Term): Option[Stat] = PartialFunction.condOpt(t) {
+      case Term.Block(_ :+ last) =>
+        last
+      case other =>
+        other
+    }
+  }
+}
+
 class ArrayToString extends SemanticRule("ArrayToString") {
+  private def check(a: Stat, pos: Position)(implicit doc: SemanticDocument): Patch = {
+    def p: Patch =
+      Patch.lint(
+        Diagnostic(
+          id = "",
+          message = "Don't use Array.toString",
+          position = pos,
+          severity = LintSeverity.Warning
+        )
+      )
+
+    if (a.symbol.value == "scala/Array.") {
+      p
+    } else {
+      a.symbol.info
+        .map(_.signature)
+        .collect {
+          case ValueSignature(tpe: TypeRef) if tpe.symbol.value == "scala/Array#" =>
+            p
+          case MethodSignature(_, _, tpe: TypeRef) if tpe.symbol.value == "scala/Array#" =>
+            p
+        }
+        .asPatch
+    }
+  }
+
   override def fix(implicit doc: SemanticDocument): Patch = {
     doc.tree.collect {
       case Term.Select(
-            a,
+            BlockLastOrSelf(a),
             s @ Term.Name("toString")
           ) =>
-        def p: Patch =
-          Patch.lint(
-            Diagnostic(
-              id = "",
-              message = "Don't use Array.toString",
-              position = s.pos,
-              severity = LintSeverity.Warning
-            )
-          )
-
-        if (a.symbol.value == "scala/Array.") {
-          p
-        } else {
-          a.symbol.info
-            .map(_.signature)
-            .collect {
-              case ValueSignature(tpe: TypeRef) if tpe.symbol.value == "scala/Array#" =>
-                p
-              case MethodSignature(_, _, tpe: TypeRef) if tpe.symbol.value == "scala/Array#" =>
-                p
-            }
-            .asPatch
-        }
+        check(a, s.pos)
+      case Term.Interpolate(
+            Term.Name("s"),
+            _,
+            values
+          ) =>
+        values.collect { case BlockLastOrSelf(a) => check(a, a.pos) }.asPatch
     }.asPatch
   }
 }
