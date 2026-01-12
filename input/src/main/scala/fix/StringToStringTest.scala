71ae91ffc336bb7508f9cf284a8ba4572d4dd3ea
xuwei-k <6b656e6a69@gmail.com>
2026-01-12 16:59:51 +0900

    Update StringToString

--- input/src/main/scala/fix/StringToStringTest.scala
+++ input/src/main/scala/fix/StringToStringTest.scala
@@ -5,4 +5,5 @@ package fix
 
 object StringToStringTest {
   def f1(s: String): String = s.toString
+  def f2(s: java.lang.String): String = s.toString
 }
--- output/src/main/scala/fix/StringToStringTest.scala
+++ output/src/main/scala/fix/StringToStringTest.scala
@@ -2,4 +2,5 @@ package fix
 
 object StringToStringTest {
   def f1(s: String): String = s
+  def f2(s: java.lang.String): String = s
 }
