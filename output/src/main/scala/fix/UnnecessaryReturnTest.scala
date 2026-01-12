d2eeb3b843f0868736f0e005ac7c2e528e8e011d
xuwei-k <6b656e6a69@gmail.com>
2026-01-12 09:31:47 +0900

    Update UnnecessaryReturn

--- output/src/main/scala/fix/UnnecessaryReturnTest.scala
+++ output/src/main/scala/fix/UnnecessaryReturnTest.scala
@@ -9,4 +9,21 @@ object UnnecessaryReturnTest {
     }
     y2
   }
+
+  def f2(x1: Boolean, x2: Int, x3: Int): Int = {
+    if (x1) {
+      x2
+    } else {
+      x3
+    }
+  }
+
+  def f3(x1: Int, x2: Int, x3: Int): Int = {
+    x1 match {
+      case 1 =>
+        x2
+      case _ =>
+        x3
+    }
+  }
 }
--- rules/src/main/scala/fix/UnnecessaryReturn.scala
+++ rules/src/main/scala/fix/UnnecessaryReturn.scala
@@ -1,6 +1,8 @@
 package fix
 
+import fix.UnnecessaryReturn.BlockLastOrSelf
 import scala.meta.Defn
+import scala.meta.Stat
 import scala.meta.Term
 import scala.meta.XtensionCollectionLikeUI
 import scala.meta.tokens.Token.KwReturn
@@ -11,29 +13,62 @@ import scalafix.XtensionSeqPatch
 import scalafix.v1.SyntacticDocument
 import scalafix.v1.SyntacticRule
 
+private object UnnecessaryReturn {
+  private object BlockLastOrSelf {
+    def unapply(t: Term): Option[Stat] = PartialFunction.condOpt(t) {
+      case Term.Block(_ :+ last) =>
+        last
+      case other =>
+        other
+    }
+  }
+}
+
 class UnnecessaryReturn extends SyntacticRule("UnnecessaryReturn") {
   override def fix(implicit doc: SyntacticDocument): Patch = {
     doc.tree.collect { case t: Defn.Def =>
       PartialFunction
         .condOpt(t.body) {
-          case Term.Block(
-                _ :+ (returnTerm: Term.Return)
-              ) =>
-            returnTerm.tokens
-              .zip(returnTerm.tokens.drop(1))
-              .collectFirst { case (r: KwReturn, s) =>
-                Seq(
-                  Patch.removeToken(r),
-                  PartialFunction
-                    .condOpt(s) { case _: Whitespace =>
-                      Patch.removeToken(s)
-                    }
-                    .asPatch
-                ).asPatch
+          case BlockLastOrSelf(returnTerm: Term.Return) =>
+            removeReturn(returnTerm)
+          case BlockLastOrSelf(t: Term.MatchLike) =>
+            t.cases
+              .map(_.body)
+              .collect { case BlockLastOrSelf(returnTerm: Term.Return) =>
+                removeReturn(returnTerm)
               }
               .asPatch
+          case BlockLastOrSelf(t: Term.If) =>
+            Seq(
+              PartialFunction
+                .condOpt(t.thenp) { case BlockLastOrSelf(returnTerm: Term.Return) =>
+                  removeReturn(returnTerm)
+                }
+                .asPatch,
+              PartialFunction
+                .condOpt(t.elsep) { case BlockLastOrSelf(returnTerm: Term.Return) =>
+                  removeReturn(returnTerm)
+                }
+                .asPatch
+            ).asPatch
         }
         .asPatch
     }.asPatch
+  }
+
+  private def removeReturn(returnTerm: Term.Return): Patch = {
+    returnTerm.tokens
+      .zip(returnTerm.tokens.drop(1))
+      .collectFirst { case (r: KwReturn, s) =>
+        Seq(
+          Patch.removeToken(r),
+          PartialFunction
+            .condOpt(s) { case _: Whitespace =>
+              Patch.removeToken(s)
+            }
+            .asPatch
+        ).asPatch
+      }
+      .asPatch
   }
 }
