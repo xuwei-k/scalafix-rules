9c122d06b77f7041bb3f484de7537239c0c9c75d
xuwei-k <6b656e6a69@gmail.com>
2026-01-12 14:39:33 +0900

    Update ArrayToString. support interpolation

--- input/src/main/scala/fix/ArrayToStringTest.scala
+++ input/src/main/scala/fix/ArrayToStringTest.scala
@@ -4,7 +4,11 @@ rule = ArrayToString
 package fix
 
 object ArrayToStringTest {
-  def int(a: Array[Int]): String = a.toString // assert: ArrayToString
+  def int(a: Array[Int]): Seq[String] = Seq(
+    a.toString, // assert: ArrayToString
+    s" $a ", // assert: ArrayToString
+    s" ${a} " // assert: ArrayToString
+  )
   def any(a: Array[Any]): String = a.toString // assert: ArrayToString
   def generic[A](a: Array[A]): String = a.toString // assert: ArrayToString
   def list(a: List[Int]): String = a.toString
@@ -12,6 +16,7 @@ object ArrayToStringTest {
   def f1: Seq[String] = Seq(
     Array(2).toString, // assert: ArrayToString
     Array("").toString, // assert: ArrayToString
+    s" ${Array(8)} ", // assert: ArrayToString
     List(3).toString,
   )
 }
