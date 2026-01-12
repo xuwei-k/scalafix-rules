8b8c05aa4b624674f4422bffcd6547581f4f186c
xuwei-k <6b656e6a69@gmail.com>
2026-01-12 09:31:47 +0900

    Update UnnecessaryReturn

--- input/src/main/scala/fix/UnnecessaryReturnTest.scala
+++ input/src/main/scala/fix/UnnecessaryReturnTest.scala
@@ -12,4 +12,21 @@ object UnnecessaryReturnTest {
     }
     return y2
   }
+
+  def f2(x1: Boolean, x2: Int, x3: Int): Int = {
+    if (x1) {
+      return x2
+    } else {
+      return x3
+    }
+  }
+
+  def f3(x1: Int, x2: Int, x3: Int): Int = {
+    x1 match {
+      case 1 =>
+        return x2
+      case _ =>
+        return x3
+    }
+  }
 }
