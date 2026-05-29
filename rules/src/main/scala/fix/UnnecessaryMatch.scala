d3e49e2e4f8e30cd71aacd38455413583847ef10
xuwei-k <6b656e6a69@gmail.com>
2026-05-29 19:43:12 +0900

    Don't apply UnnecessaryMatch if PolyFunction

--- rules/src/main/scala/fix/UnnecessaryMatch.scala
+++ rules/src/main/scala/fix/UnnecessaryMatch.scala
@@ -11,11 +11,18 @@ import scalafix.v1.XtensionSeqPatch
 
 class UnnecessaryMatch extends SyntacticRule("UnnecessaryMatch") {
   override def fix(implicit doc: SyntacticDocument): Patch = {
+    val polyFunctionBody = doc.tree.collect { case p: Term.PolyFunction =>
+      p.body
+    }.collect { case f: Term.Function =>
+      f.pos
+    }.toSet
     doc.tree.collect {
       case f @ Term.Function.After_4_6_0(
             Term.ParamClause(Term.Param(_, Term.Name(p1), _, _) :: Nil, _),
             Term.Match.After_4_9_9(Term.Name(p2), Term.CasesBlock(cases), _)
-          ) if p1 == p2 && cases.forall(_.collect { case Term.Name(n) if n == p2 => () }.isEmpty) =>
+          ) if p1 == p2 && cases.forall(_.collect {
+            case Term.Name(n) if n == p2 => ()
+          }.isEmpty) && !polyFunctionBody(f.pos) =>
         Seq(
           Patch.removeTokens(f.tokens.takeWhile(!_.is[Token.KwMatch])),
           Patch.removeTokens(f.tokens.find(_.is[Token.KwMatch]))
