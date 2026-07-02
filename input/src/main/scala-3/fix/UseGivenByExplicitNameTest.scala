00b6a7e85c2d70fdae6ec82a083a9dcd35a7dca0
xuwei-k <6b656e6a69@gmail.com>
2026-07-02 17:22:47 +0900

    UseGivenByExplicitName

---
+++ input/src/main/scala-3/fix/UseGivenByExplicitNameTest.scala
@@ -0,0 +1,28 @@
+/*
+rule = UseGivenByExplicitName
+ */
+package fix
+
+object UseGivenByExplicitNameTest {
+  given a1: Int = 2
+  val a2: Int = 3
+  given a3[A]: List[A] = Nil
+  given Int = 4
+
+  def a4(using x1: Int): Int = {
+    x1 - 9
+  }
+
+  class B1(using x1: Int) {
+    def f: Int = x1 * 2
+  }
+
+  def f1: Int = UseGivenByExplicitNameTest.a1 // assert: UseGivenByExplicitName
+
+  def f2: Int = UseGivenByExplicitNameTest.a2
+
+  import UseGivenByExplicitNameTest.a1 // assert: UseGivenByExplicitName
+  import UseGivenByExplicitNameTest.a1 as c1 // assert: UseGivenByExplicitName
+  import UseGivenByExplicitNameTest.a2
+  import UseGivenByExplicitNameTest.a3 // assert: UseGivenByExplicitName
+}
