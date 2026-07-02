0533a19b1bd7508a3202cb64fda447ff2eef0e42
xuwei-k <6b656e6a69@gmail.com>
2026-07-02 17:22:47 +0900

    UseGivenByExplicitName

---
+++ input/src/main/scala-3/fix/UseGivenByExplicitNameTest.scala
@@ -0,0 +1,20 @@
+/*
+rule = UseGivenByExplicitName
+ */
+package fix
+
+object UseGivenByExplicitNameTest {
+  given a1: Int = 2
+  val a2: Int = 3
+  given a3[A]: List[A] = Nil
+  given Int = 4
+
+  def f1: Int = UseGivenByExplicitNameTest.a1 // assert: UseGivenByExplicitName
+
+  def f2: Int = UseGivenByExplicitNameTest.a2
+
+  import UseGivenByExplicitNameTest.a1 // assert: UseGivenByExplicitName
+  import UseGivenByExplicitNameTest.a1 as c1 // assert: UseGivenByExplicitName
+  import UseGivenByExplicitNameTest.a2
+  import UseGivenByExplicitNameTest.a3 // assert: UseGivenByExplicitName
+}
---
+++ rules/src/main/scala/fix/UseGivenByExplicitName.scala
@@ -0,0 +1,35 @@
+package fix
+
+import scala.meta._
+import scalafix.lint.LintSeverity
+import scalafix.v1._
+
+class UseGivenByExplicitName extends SemanticRule("UseGivenByExplicitName") {
+  override def fix(implicit doc: SemanticDocument): Patch = {
+    val givenDefinePositions: Set[(Int, Int)] =
+      doc.tree.collect { case x: Defn.GivenAlias =>
+        (x.name.pos.start, x.name.pos.end)
+      }.toSet
+
+    doc.tree.collect {
+      case t: Term.Name if !givenDefinePositions((t.pos.start, t.pos.end)) && t.symbol.info.exists(_.isGiven) =>
+        Seq(t)
+      case i: Import =>
+        i.importers.flatMap(_.importees).collect {
+          case x: Importee.Name if x.name.symbol.info.exists(_.isGiven) =>
+            x
+          case x: Importee.Rename if x.name.symbol.info.exists(_.isGiven) =>
+            x
+        }
+    }.flatten.map { t =>
+      Patch.lint(
+        Diagnostic(
+          id = "",
+          message = "Don't use given by name",
+          position = t.pos,
+          severity = LintSeverity.Warning
+        )
+      )
+    }.asPatch
+  }
+}
