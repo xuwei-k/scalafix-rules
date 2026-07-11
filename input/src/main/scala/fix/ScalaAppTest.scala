9b9eaa842970c10af8c4bd1520b599e15539957d
xuwei-k <6b656e6a69@gmail.com>
2026-07-11 17:25:37 +0900

    Fix ScalaApp TODO

--- input/src/main/scala/fix/ScalaAppTest.scala
+++ input/src/main/scala/fix/ScalaAppTest.scala
@@ -13,10 +13,22 @@ object ScalaAppTest1 extends App {
   class X
 }
 
-trait ScalaAppTestTrait1
+trait ScalaAppTestTrait1 {
+  def a1: Int
+  def a2: Int
+  protected def a3: Int
+}
+
 trait ScalaAppTestTrait2
 
 object ScalaAppTest2 extends App with ScalaAppTestTrait1 with ScalaAppTestTrait2 {
+
+  override def a1 = 1
+  override val a2 = 2
+  protected def a3 = 3
+  final val a4 = 4
+  private val a5 = 5
+  val a6 = 6
 
   println("a")
 
--- output/src/main/scala/fix/ScalaAppTest.scala
+++ output/src/main/scala/fix/ScalaAppTest.scala
@@ -14,13 +14,31 @@ object ScalaAppTest1   {
   class X
 }
 
-trait ScalaAppTestTrait1
+trait ScalaAppTestTrait1 {
+  def a1: Int
+  def a2: Int
+  protected def a3: Int
+}
+
 trait ScalaAppTestTrait2
 
 object ScalaAppTest2 extends   ScalaAppTestTrait1 with ScalaAppTestTrait2 {
 
+  
+  
+  
+  
+  
   def main(args: Array[String]): Unit = {
+  val a6 = 6
+
   println("a")
   }
 
+
+  override def a1 = 1
+  override val a2 = 2
+  protected def a3 = 3
+  final val a4 = 4
+  private val a5 = 5
 }
