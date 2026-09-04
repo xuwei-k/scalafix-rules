8cbfe56256b36f94e521282104830f11dbc8ae07
xuwei-k <6b656e6a69@gmail.com>
2026-09-04 20:16:23 +0900

    Fix MapSequenceTraverse

--- output/src/main/scala/fix/MapSequenceTraverseTest.scala
+++ output/src/main/scala/fix/MapSequenceTraverseTest.scala
@@ -6,4 +6,9 @@ object MapSequenceTraverseTest {
   def withParens: Option[List[Int]] = List(1, 2, 3).traverse(a => if (a % 2 == 0) Some(a) else None)
   def withBraces: Option[List[Int]] = List(1, 2, 3).traverse{ a => if (a % 2 == 0) Some(a) else None }
   def withParensThenBraces: Option[List[Int]] = List(1, 2, 3).traverse{ a => if (a % 2 == 0) Some(a) else None }
+
+  def f1: List[Vector[Option[Either[Boolean, Int]]]] =
+    Vector(Option(2)).map { x => x.traverse(f2) }.sequence
+
+  private def f2[A](x: A): List[Either[Boolean, A]] = Nil
 }
